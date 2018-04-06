a70e38e913f18818ed0066b3757fb000801cf91e
yulken <caiocftorres@gmail.com>
2018-04-06 12:31:14 -0300

    Removendo diretórios antigos

--- js/nav.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.nav-link').click(function() {
-    var selected = $('.active.nav-link');
-    selected.removeClass('active');
-    $(this).addClass('active');
-
-  });
-});
